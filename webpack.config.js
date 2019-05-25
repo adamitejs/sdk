540e31762845d413be44cbb5b98bdd2803d4887c
Jesse Dunlap <jesse@hydrais.com>
2019-05-24 21:32:25 -0500

    Added code for creating a umd bundle

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./index.ts",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"]
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    library: "Adamite",
+    libraryTarget: "umd",
+    filename: "adamite.js"
+  }
+};
